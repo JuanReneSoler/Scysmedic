88abfc5a43a88acf31a0ea7164935d49e8f468c8
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2020-08-03 23:42:47 -0400

    Script completo de la base de datos

---
+++ Sql/inserts.sql
@@ -0,0 +1,18 @@
+
+use ScysmedicDB
+
+insert into Farmacias.TipoMedicamento values('Blister');
+insert into Farmacias.TipoMedicamento values ('Jarabe');
+insert into Shared.TipoPago values ('Targeta');
+insert into Shared.TipoPago values ('Efectivo');
+insert into Shared.MotivoDevolucion values ('Producto danado');
+insert into Hospitales.Especialidad values ('Otorrinolaringologo');
+insert into Hospitales.Especialidad values ('Infectologo');
+insert into Hospitales.Especialidad values ('Dermatologo');
+insert into Hospitales.Especialidad values ('Otorrino');
+insert into Hospitales.Especialidad values ('Pediatra');
+insert into Hospitales.Especialidad values ('Ortopeda');
+insert into Hospitales.Especialidad values ('Oculista');
+
+
+
